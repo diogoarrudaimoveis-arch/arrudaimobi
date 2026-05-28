ffc16aa7eba526bd57cd46c14e64515d9fbf38e5
Mavis AI <mavis@arruda.auto>
2026-05-28 22:16:54 +0000

    Fix route order: /captar-imovel before /captar for proper matching

--- src/App.tsx
+++ src/App.tsx
@@ -106,8 +106,8 @@ function App() {
                           <Route path="/reset-password" element={<ResetPassword />} />
                           <Route path="/termos" element={<TermsOfService />} />
                           <Route path="/privacidade" element={<PrivacyPolicy />} />
-                          <Route path="/captar" element={<CaptarImovel />} />
                           <Route path="/captar-imovel" element={<CaptarImovel />} />
+                          <Route path="/captar" element={<CaptarImovel />} />
 
                           {/* Proprietário portal */}
                           <Route path="/proprietario" element={<ProtectedRoute requireOwner><ProprietarioDashboard /></ProtectedRoute>} />
